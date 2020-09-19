7ca216ff85eafcbf99bc93769848fd13af19923c
De-Great Yartey <yarteydegreat2@gmail.com>
2020-09-19 21:40:47 +0000

    Initial commit

---
+++ static/main.js
@@ -0,0 +1,18 @@
+let cursor = 0
+
+// populate this array with 10 images
+const galleryItems = [
+  {
+    src: "/images/totem.jpg",
+    comments: [
+      {
+        user: "blackmann",
+        comment: "This picture is so dope boyy!",
+      }
+    ],
+    caption: "Totem is a 3d art made by De-Great using Blender and Photoshop. This art was inspired by Matt Dixon Transmission series.",
+  },
+]
+
+// put in your logic here
+// This video might be of help: https://youtu.be/-NLu31FGz7s
